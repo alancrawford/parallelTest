228f78863bf4f5124484edba573d0f2ee8114111
alancrawford <a.r.crawford@gmail.com>
2020-02-28 17:43:44 +0100

    Push before heading home, need to test out myriad.

--- julia/ucl-myriad/submit_script.sh
+++ julia/ucl-myriad/submit_script.sh
@@ -1,22 +1,56 @@
+#!/bin/bash -l
+
 echo "starting qsub script file"
 source ~/.bash_profile
 date
 
 # Load any modules here
-# module load ....
+module unload compilers
+module load compilers/gnu/4.9.2
+module load knitro/12.0.0/gnu-4.9.2
 
 # here's the SGE directives
 # ------------------------------------------
-#$ -q batch.q   # <- the name of the Q you want to submit to
-#$ -pe julia 20  #  <- load the julia parallel env w/ $(arg1) slots
-#$ -S /bin/bash   # <- run the job under bash
-#$ -N JuliaInParallel_Test # <- name of the job in the qstat output
-#$ -o timer.out # direct output stream to here
-#$ -e timer.err # <- name of the stderr file.
-#$ -cwd  		# <- write to current working directory
-
-awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > hosts
+#$ -pe mpi 4
+#$ -N JuliaInParallel_Test 	# <- name of the job in the qstat output
+#$ -o timer.out 			# direct output stream to here
+#$ -e timer.err 			# <- name of the stderr file.
+#$ -cwd  					# <- write to current working directory
 
 echo "calling julia now:"
 
-julia --machinefile hosts -L ../incl.jl sge.jl > results_test.log
+julia10 --depwarn=no --machinefile $PE_HOSTFILE -L ../incl.jl sge.jl > results_test.log
+
+
+
+# https://www.rc.ucl.ac.uk/docs/Example_Jobscripts/
+
+# #!/bin/bash -l
+
+# Batch script to run a serial array job under SGE.
+
+# Request ten minutes of wallclock time (format hours:minutes:seconds).
+#$ -l h_rt=0:10:0
+
+# Request 1 gigabyte of RAM (must be an integer followed by M, G, or T)
+# #$ -l mem=1G
+
+# Request 15 gigabyte of TMPDIR space (default is 10 GB)
+# #$ -l tmpfs=15G
+
+# Set up the job array.  In this instance we have requested 10000 tasks
+# numbered 1 to 10000.
+# #$ -t 1-10000
+
+# Set the name of the job.
+# #$ -N MyArrayJob
+
+# Set the working directory to somewhere in your scratch space. 
+# Replace "<your_UCL_id>" with your UCL user ID :)
+# #$ -wd /home/<your_UCL_id>/Scratch/output
+
+# Run the application.
+
+# echo "$JOB_NAME $SGE_TASK_ID"
+
+
