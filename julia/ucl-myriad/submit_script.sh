40f0ab35e07b5de417ee76cceb99ab37bc6c412d
alancrawford <a.r.crawford@gmail.com>
2020-03-02 17:56:25 +0100

    testfile troubleshooting

--- julia/ucl-myriad/submit_script.sh
+++ julia/ucl-myriad/submit_script.sh
@@ -16,10 +16,13 @@ module unload compilers
 module load compilers/gnu/4.9.2
 module load knitro/12.0.0/gnu-4.9.2
 
+awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > hosts
+
 echo "calling julia now on:"
 
 cat $PE_HOSTFILE
+cat hosts
 
-/shared/ucl/apps/julia/1.0.0/bin/julia --depwarn=no --machine-file $PE_HOSTFILE -L ../incl.jl sge.jl > results_test.log
+/shared/ucl/apps/julia/1.0.0/bin/julia --depwarn=no --machine-file hosts -L ../incl.jl sge.jl > results_test.log
 
 
