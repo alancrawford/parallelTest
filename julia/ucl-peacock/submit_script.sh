6524768f98964f4e8d386f532f3ccb07f307abb6
Alan Crawford <a.crawford.11@ucl.ac.uk>
2020-04-03 12:10:15 +0100

    Peacock test

--- julia/ucl-peacock/submit_script.sh
+++ julia/ucl-peacock/submit_script.sh
@@ -1,11 +1,15 @@
-#!/bin/bash -l
+source ~/.bash_profile
+
 #$ -l tmem=1G
 #$ -l h_vmem=1G
 #$ -l h_rt=1:0:0
 #$ -S /bin/bash
 #$ -j y
-#$ -N MyTESTJOBNAME
+#$ -wd ~/sgelogs
+#$ -N JuliaMPItest
 #$ -pe mpi 12
+#$ -e JuliaMPItest.err
+#$ -o JuliaMPItest.out
 
 awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > hosts
 
@@ -14,6 +18,6 @@ echo "calling julia now on:"
 cat $PE_HOSTFILE
 cat hosts
 
-julia11 --depwarn=no --machine-file hosts -L ../incl.jl sge.jl > results_test.log
+julia11 --depwarn=no --machine-file hosts -L ../incl.jl sge.jl > JuliaMPItest.log
 
 echo "I am done."
--- julia/ucl-peacock/submit_script_tasks.sh
+++ julia/ucl-peacock/submit_script_tasks.sh
@@ -1,13 +1,15 @@
-#!/bin/bash -l
+source ~/.bash_profile
+
 #$ -l tmem=1G
 #$ -l h_vmem=1G
 #$ -l h_rt=1:0:0
 #$ -S /bin/bash
 #$ -j y
-#$ -N MyTESTJOBNAME
+#$ -wd ~/sgelogs
+#$ -N JuliaTASKStest
 #$ -t 1-12
-#$ -e MyTEST_rasks.err
-#$ -o MyTEST_rasks.out
+#$ -e JuliaTASKStest.err
+#$ -o JuliaTASKStest.out
 
 awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > hosts_tasks
 
@@ -16,6 +18,6 @@ echo "calling julia now on:"
 cat $PE_HOSTFILE
 cat hosts_tasks
 
-julia11 --depwarn=no --machine-file hosts_tasks -L ../incl.jl sge.jl > results_test_tasks.log
+julia11 --depwarn=no --machine-file hosts_tasks -L ../incl.jl sge.jl > JuliaTASKStest.log
 
 echo "I am done."
