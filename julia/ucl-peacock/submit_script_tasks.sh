5e22884736335b4977bc41d154e4c2b9f04a1216
Alan Crawford <a.crawford.11@ucl.ac.uk>
2020-04-03 11:52:16 +0100

    Test Peacock

---
+++ julia/ucl-peacock/submit_script_tasks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#$ -l tmem=1G
+#$ -l h_vmem=1G
+#$ -l h_rt=1:0:0
+#$ -S /bin/bash
+#$ -j y
+#$ -N MyTESTJOBNAME
+#$ -t 1-12
+
+awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > hosts
+
+echo "calling julia now on:"
+
+cat $PE_HOSTFILE
+cat hosts
+
+julia11 --depwarn=no --machine-file hosts -L ../incl.jl sge.jl > results_test.log
+
+echo "I am done."
