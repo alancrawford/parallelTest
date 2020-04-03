8feed2157e29a8c7b232cd16783c3f74600140a2
Alan Crawford <a.crawford.11@ucl.ac.uk>
2020-04-03 11:57:55 +0100

    Test Peacock

--- julia/ucl-peacock/submit_script_tasks.sh
+++ julia/ucl-peacock/submit_script_tasks.sh
@@ -9,13 +9,13 @@
 #$ -e MyTEST_rasks.err
 #$ -o MyTEST_rasks.out
 
-awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > hosts
+awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > hosts_tasks
 
 echo "calling julia now on:"
 
 cat $PE_HOSTFILE
-cat hosts
+cat hosts_tasks
 
-julia11 --depwarn=no --machine-file hosts -L ../incl.jl sge.jl > results_test_tasks.log
+julia11 --depwarn=no --machine-file hosts_tasks -L ../incl.jl sge.jl > results_test_tasks.log
 
 echo "I am done."
